425d5ed159f559de0213d148c5bc942bb95a604e
AQLT <alain.quartier@yahoo.fr>
2019-12-24 18:53:29 +0100

    itineraire

--- Code/ratp/include/itineraire.h
+++ Code/ratp/include/itineraire.h
@@ -4,6 +4,8 @@
 #include <vector>
 #include "Arret.h"
 #include <iostream>
+#include "Node.h"
+#include "Edge.h"
 using namespace std;
 
 
@@ -12,11 +14,24 @@ class itineraire
     public:
         itineraire();
         virtual ~itineraire();
+        Node* ExtractSmallest(vector<Node*>& nodes);
+        bool Contains(vector<Node*>& nodes, Node* node);
+        vector<Node*>* AdjacentRemainingNodes(Node* node);
+        int Distance(Node* node1, Node* node2);
+        void PrintShortestRouteTo(Node* destination);
+        void Dijkstras();
+        vector<Edge*>* AdjacentEdges(vector<Edge*>& edges, Node* node);
+        void RemoveEdge(vector<Edge*>& edges, Edge* edge);
+        int getIndiceFromNode(std::string identifiant ,vector<Node*> les_noeuds);
+
 
     protected:
 
     private:
         vector<Arret*> liste_arrets;
+        vector<Node*> nodes;
+        //vector<Edge*> edges;
+        Edge** edges;
 
 };
 
--- Code/ratp/src/itineraire.cpp
+++ Code/ratp/src/itineraire.cpp
@@ -12,3 +12,190 @@ itineraire::~itineraire()
 {
     //dtor
 }
+
+Node* itineraire::ExtractSmallest(vector<Node*>& nodes)
+{
+    int size = nodes.size();
+    if (size == 0)
+        return NULL;
+    int smallestPosition = 0;
+    Node* smallest = nodes.at(0);
+    for (int i=1; i<size; ++i)
+    {
+        Node* current = nodes.at(i);
+        if (current->distanceFromStart <
+                smallest->distanceFromStart)
+        {
+            smallest = current;
+            smallestPosition = i;
+        }//
+    }
+    nodes.erase(nodes.begin() + smallestPosition);
+    return smallest;
+}
+
+bool itineraire::Contains(vector<Node*>& nodes, Node* node)
+{
+    const int size = nodes.size();
+    for(int i=0; i<size; ++i)
+    {
+        if (node == nodes.at(i))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+vector<Node*>* itineraire::AdjacentRemainingNodes(Node* node)
+{
+    vector<Node*>* adjacentNodes = new vector<Node*>();
+    const int size = edges.size();
+    for(int i=0; i<size; ++i)
+    {
+        Edge* edge = edges.at(i);
+        Node* adjacent = NULL;
+        if (edge->node1 == node)
+        {
+            adjacent = edge->node2;
+        }
+        //else if (edge->node2 == node) //pas dans les deux sens
+        //{
+         //   adjacent = edge->node1;
+        //}
+        if (adjacent && Contains(nodes, adjacent))
+        {
+            adjacentNodes->push_back(adjacent);
+        }
+    }
+    return adjacentNodes;
+}
+
+int itineraire::Distance(Node* node1, Node* node2)
+{
+    const int size = edges.size();
+    for(int i=0; i<size; ++i)
+    {
+        Edge* edge = edges.at(i);
+        if (edge->Connects(node1, node2))
+        {
+            return edge->distance;
+        }
+    }
+    return -1; // should never happen
+}
+
+void PrintShortestRouteTo(Node* destination)
+{
+    Node* previous = destination;
+    cout << "Temps de trajet : "
+         << destination->distanceFromStart << endl;
+    while (previous)
+    {
+        cout << previous->id << " ";
+        previous = previous->previous;
+    }
+    cout << endl;
+}
+
+void Dijkstras()
+{
+    while (nodes.size() > 0)
+    {
+        Node* smallest = ExtractSmallest(nodes);
+        vector<Node*>* adjacentNodes =
+            AdjacentRemainingNodes(smallest);
+        const int size = adjacentNodes->size();
+        for (int i=0; i<size; ++i)
+        {
+            Node* adjacent = adjacentNodes->at(i);
+            int distance = Distance(smallest, adjacent) +
+                           smallest->distanceFromStart;
+//vector<Edge*>& Edges
+            if (distance < adjacent->distanceFromStart)
+            {
+                adjacent->distanceFromStart = distance;
+                adjacent->previous = smallest;
+            }
+        }
+        delete adjacentNodes;
+    }
+}
+
+// these two not needed
+vector<Edge*>* AdjacentEdges(vector<Edge*>& edges, Node* node)
+{
+    vector<Edge*>* adjacentEdges = new vector<Edge*>();
+    const int size = edges.size();
+    for(int i=0; i<size; ++i)
+    {
+        Edge* edge = edges.at(i);
+        if (edge->node1 == node)
+        {
+            cout << "adjacent: " << edge->node2->id << endl;
+            adjacentEdges->push_back(edge);
+        }
+        else if (edge->node2 == node)
+        {
+            cout << "adjacent: " << edge->node1->id << endl;
+            adjacentEdges->push_back(edge);
+        }
+    }
+    return adjacentEdges;
+}
+void RemoveEdge(vector<Edge*>& edges, Edge* edge)
+{
+    vector<Edge*>::iterator it;
+    for (it=edges.begin(); it<edges.end(); ++it)
+    {
+        if (*it == edge)
+        {
+            edges.erase(it);
+            return;
+        }
+    }
+}
+
+int getIndiceFromNode(std::string identifiant ,vector<Node*> les_noeuds)
+{
+
+ int indice_sortie;
+ const int size = les_noeuds.size();
+ for(int i=0; i<size; ++i){
+ //       cout << "identifiant noeud : " << les_noeuds[i]->id << endl;
+ //       cout << "identifiant : " << identifiant << endl;
+        if(les_noeuds[i]->id== identifiant){
+            indice_sortie = i;
+        }
+
+ }
+
+
+ return(indice_sortie);
+
+}
+
+std::vector<std::string> split(std::string str,std::string sep)
+{
+    char* cstr=const_cast<char*>(str.c_str());
+    char* current;
+    std::vector<std::string> arr;
+    current=strtok(cstr,sep.c_str());
+    while(current!=NULL)
+    {
+        arr.push_back(current);
+        current=strtok(NULL,sep.c_str());
+    }
+    return arr;
+}
+
+DijkstrasFinal(vector<Node*> nodes, Edge** edges, std::string entree,std::string sortie){ //vector<Node*> nodes, Edge** edges,
+
+int entree_int = getIndiceFromNode(entree,nodes);
+int sortie_int = getIndiceFromNode(sortie,nodes);
+
+nodes[entree_int]->distanceFromStart = 0; // set start node
+Dijkstras();
+PrintShortestRouteTo(nodes[sortie_int]);
+
+}
